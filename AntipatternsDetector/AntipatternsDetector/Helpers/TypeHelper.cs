78115bedbbb5787b33fb9a17363f038ab055bd68
m.andreykov <m.andreykov@cian.ru>
2024-04-06 23:58:19 +0300

    Minor changes

---
+++ AntipatternsDetector/AntipatternsDetector/Helpers/TypeHelper.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using Microsoft.CodeAnalysis;
+
+namespace AntipatternsDetector.Helpers;
+
+public static class TypeHelper
+{
+    public static bool Implements(this ITypeSymbol typeSymbol, ITypeSymbol type)
+    {
+        return typeSymbol != null &&
+               typeSymbol.AllInterfaces.Any(symbol => symbol.ConstructedFrom.Equals(type));
+    }
+    
+    public static bool Implements(this ITypeSymbol typeSymbol, KnownType type)
+    {
+        return typeSymbol != null &&
+               typeSymbol.AllInterfaces.Any(symbol => symbol.ConstructedFrom.Is(type));
+    }
+    
+    public static bool Is(this ITypeSymbol typeSymbol, KnownType type)
+    {
+        return typeSymbol != null && IsMatch(typeSymbol, type);
+    }
+    
+    private static bool IsMatch(ITypeSymbol typeSymbol, KnownType type)
+    {
+        return type.Matches(typeSymbol.SpecialType) ||
+               type.Matches(typeSymbol.OriginalDefinition.SpecialType) ||
+               type.Matches(typeSymbol.ToDisplayString()) ||
+               type.Matches(typeSymbol.OriginalDefinition.ToDisplayString());
+    }
+}
